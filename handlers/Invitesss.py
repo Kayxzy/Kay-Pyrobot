bb3ec24894379036d111e0e55f272bb4070e414c
Kayxzy <106707683+Kayxzy@users.noreply.github.com>
2022-10-02 06:25:54 +0700

    Update Invitesss.py

--- handlers/Invitesss.py
+++ handlers/Invitesss.py
@@ -5,12 +5,12 @@ import asyncio
 
 @Client.on_message(filters.command(["inviteall", "kidnapall"], [".", "!", "/"]) & filters.user(SUDO_USERS))
 async def inviteall(client: Client, message: Message):
-    zaid = await message.reply_text("⚡ Gime Title also\n ex: /inviteall @testing")
+    kay = await message.reply_text("⚡ Gime Title also\n ex: /inviteall @testing")
     text = message.text.split(" ", 1)
     queryy = text[1]
     chat = await client.get_chat(queryy)
     tgchat = message.chat
-    await zaid.edit_text(f"inviting users from {chat.username}")
+    await kay.edit_text(f"inviting users from {chat.username}")
     async for member in client.iter_chat_members(chat.id):
         user= member.user
         zxb= ["online", "offline" , "recently", "within_week"]
